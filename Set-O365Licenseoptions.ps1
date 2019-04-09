43a424857119c5def13f8c1814c0fe511a994995
Thom Brink <thombrink@users.noreply.github.com>
2019-04-09 17:15:14 +0200

    Update Set-O365Licenseoptions.ps1
    spaces

--- Set-O365Licenseoptions.ps1
+++ Set-O365Licenseoptions.ps1
@@ -13,14 +13,14 @@ Function Changelicenseoption {
     $MBX = Get-Msoluser -All | Where-Object {$_.UserPrincipalname -like "NL*"}
     $LicensesRemoved = 0
     ForEach ($M in $MBX) {
-        Write-Host "Checking licenses for" $M.Userprincipalname
+        Write-Host "Checking licenses for " $M.Userprincipalname
         $accountsku1 = (Get-MsolAccountSku).AccountSkuID -split ":"
         $tenantname = $accountsku1[0]
         if ((Get-MsolUser -UserPrincipalName $M.UserPrincipalName).Licenses.AccountSkuID -like "*$license") {
             $accountsku = "$tenantname" + ':' + "$license"
             $lo = New-MsolLicenseOptions -AccountSkuId $accountsku -DisabledPlans $disabledplan
             Set-MsolUserLicense -UserPrincipalName $M.UserPrincipalName -LicenseOptions $lo
-            Write-Host $license "is aangepast voor" $m.Userprincipalname -ForegroundColor Green
+            Write-Host $license " is aangepast voor " $m.Userprincipalname -ForegroundColor Green
         }
         else {
             Write-Host "Licenties komen niet overeen met $license" -ForegroundColor Red
